565f31a5f8167e603a4ff177e881dbccb9241faf
jlee <john1997@live.ca>
2019-01-26 22:25:39 -0500

    MongoDB setup

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
+//Express Boilerplate code
 let express = require('express');
 let app = express();
 let port = 3000;
 
+//MongoDB Boilerplate code
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
+
+//Endpoint redirection code
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
