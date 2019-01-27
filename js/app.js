2d55379929de48f73bfcb23cca35b76a931efc94
jlee <john1997@live.ca>
2019-01-26 22:51:39 -0500

    Backend mongodb set up + document insertion works

--- js/app.js
+++ js/app.js
@@ -4,14 +4,28 @@ let app = express();
 let port = 3000;
 
 //MongoDB Boilerplate code
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
+let MongoClient = require('mongodb').MongoClient;
+let url = "mongodb://localhost:27017/mydb";
 
 MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
+    if (err) throw err;
+    let dbo = db.db("mydb");
+    dbo.createCollection("faces", function(err, res) {
+      if (err) throw err;
+      console.log("Collection created!");
+      addFace(dbo);
+      db.close();
+    });
 });
+//Function for adding a new face to the DB
+function addFace(dbo){
+    let newFace = {test: "data1", secondTest: "data2"};
+    dbo.collection("faces").insertOne(newFace, function(err, res){
+        if (err) throw err;
+        console.log("1 document inserted");
+    })
+}
+
 
 //Endpoint redirection code
 app.get('/', (req, res) => res.send('Hello World!'));
